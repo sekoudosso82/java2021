72f8402c89d6bcb22540f63976730b59039c4e9d
YOUR NAME <YOUR@EMAIL.com>
2021-03-29 17:17:19 -0400

    first hello word

---
+++ src/IntroToJava.java
@@ -0,0 +1,10 @@
+public class IntroToJava {
+    public static void message(String myString, int myInt){
+        System.out.println("This  is my string: "+ myString);
+        System.out.println("and this is my int: "+myInt);
+    }
+
+    public static void main(String[] args) {
+        message("Hello word", 4);
+    }
+}
