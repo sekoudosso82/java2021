20812e2ac039a2c74b9a9a1acf51d8b65e07026c
YOUR NAME <YOUR@EMAIL.com>
2021-03-30 10:05:22 -0400

    loop and string methods

---
+++ src/stringPakage/StringMethods.java
@@ -0,0 +1,22 @@
+package stringPakage;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String string1  = "hello";
+        String string2 = new String("Hello");
+
+        System.out.println("String.length():\t\t" + string1.length());
+        System.out.println("String.toUpperCase:\t\t" + string1.toUpperCase());
+        System.out.println("String.toLowerCase:\t\t" + string1.toLowerCase());
+        System.out.println("String.indexOf('e'):\t\t" + string1.indexOf('e'));
+        System.out.println("String.lastIndexOf('l'):\t\t" + string1.lastIndexOf('l'));
+        System.out.println("String.charAt(4):\t\t" + string1.charAt(4));
+        System.out.println("String.equals(string2):\t\t" + string1.equals(string2));
+        System.out.println("String.equalsIgnoreCase(string2):\t" + string1.equalsIgnoreCase(string2));
+
+
+
+
+
+    }
+}
